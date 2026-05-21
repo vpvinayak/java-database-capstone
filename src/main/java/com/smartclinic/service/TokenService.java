a6137f7ae3e80478ece913094209a3fed2ed238c
Vinayak Patel <139354807+vpvinayak@users.noreply.github.com>
2026-05-21 06:29:59 +0530

    Update TokenService.java

--- src/main/java/com/smartclinic/service/TokenService.java
+++ src/main/java/com/smartclinic/service/TokenService.java
@@ -1,1 +1,31 @@
-package com.smartclinic.service; public class TokenService {}
+package com.smartclinic.service;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
+
+public class TokenService {
+
+    private final String secret =
+            "SmartClinicSecretKeySmartClinicSecretKey";
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(
+                        new Date(
+                                System.currentTimeMillis()
+                                        + 86400000))
+                .signWith(getSigningKey(),
+                        SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public Key getSigningKey() {
+        return Keys.hmacShaKeyFor(
+                secret.getBytes());
+    }
+}
